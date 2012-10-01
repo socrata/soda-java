072d6d6eaab68939d81b60db2d074882ef6c173d
Will <willpugh@gmail.com>
2012-09-30 23:38:35 -0700

    WHAT:   1)  Added more meat to the README   2)  Created a unit test to run through the examples in the README

--- README.md
+++ README.md
@@ -1,4 +1,73 @@
 soda-java
 =========
 
-This is the Java API for the SODA2 API
+This is the Java API for SODA2.  SODA is broken in to the producer and consumer parts of the API.  The consumer
+interface is very SQL like.  This API provides the Soda2Consumer interface, along with a builder pattern for creating
+queries.  It also provides the Soda2Producer interface for updating data sets.
+
+This library is built using two "Layers" or interaction.  The HttpLowLevel class takes care of much of the common
+HTTP and URL wrangling required by SODA, but does not deal with any unmarshalling of results.
+
+The Soda2Consumer and Soda2Producer classes are built on top of the HttpLowLevel class.  They build upon it to
+use Jackson to do marshalling from the JSON to Java Objects.
+
+Consumer
+--------
+
+**Simple Query**
+The consumer API is simple.  The following example will issue two requests, one will return the results from the "test-data" dataset,
+as the JSON string.  The other will return the results as the `Nomination` java objects:
+
+    Soda2Consumer consumer = Soda2Consumer.newConsumer("https://sandbox.demo.socrata.com", "testuser@gmail.com", "OpenData", "D8Atrg62F2j017ZTdkMpuZ9vY");
+
+    //To get a raw String of the results
+    ClientResponse response = consumer.getHttpLowLevel().query("nominationsCopy", HttpLowLevel.JSON_TYPE, SoqlQuery.SELECT_ALL);
+    String payload = response.getEntity(String.class);
+    System.out.println(payload);
+
+    //Get get this automatcally serialized into a set of Java Beans annotated with Jackson JOSN annotations
+    List<Nomination> nominations = consumer.query("nominationsCopy", SoqlQuery.SELECT_ALL, Nomination.LIST_TYPE);
+    TestCase.assertTrue(nominations.size() > 0);
+    System.out.println(nominations.size());
+
+
+**Building Queries**
+Along with the consumer API, is a builder class to make it easier to build the SoQL queries.  For example, to query for the name, position and nomination date of
+nominees for the Department of State, sorted by position:
+
+    //Create a SoQL query to find the nominations for the Department of State
+    SoqlQuery   departmentOfStateQuery = new SoqlQueryBuilder()
+            .addSelectPhrase("name")
+            .addSelectPhrase("position")
+            .addSelectPhrase("nomination_date")
+            .setWhereClause("agency_name='Department of State'")
+            .addOrderByPhrase(new OrderByClause(SortOrder.Descending, "position"))
+            .build();
+    nominations = consumer.query("nominationsCopy", departmentOfStateQuery, Nomination.LIST_TYPE);
+
+
+Producer
+--------
+
+**CRUD on Objects**
+SODA2 also provides mechanisms for creating, updating or deleting individual rows
+
+    Soda2Producer producer = Soda2Producer.newProducer("https://sandbox.demo.socrata.com", "testuser@gmail.com", "OpenData", "D8Atrg62F2j017ZTdkMpuZ9vY");
+
+    //Get get this automatically serialized into a set of Java Beans annotated with Jackson JOSN annotations
+    Meta nominationAddedMeta = producer.addObject("testupdate", NOMINATION_TO_ADD);
+
+    //Update the nomination
+    Meta nominationUpdatedMeta = producer.update("testupdate", nominationAddedMeta.getId(), NOMINATION_TO_UPDATE);
+
+    //Delete the nomination
+    producer.delete("testupdate", nominationUpdatedMeta.getId());
+
+
+**Upsert based on a stream**
+The library also allows callers to upsert based on a CSV file or stream.
+
+    Soda2Producer producer = Soda2Producer.newProducer("https://sandbox.demo.socrata.com", "testuser@gmail.com", "OpenData", "D8Atrg62F2j017ZTdkMpuZ9vY");
+
+    InputStream inputStream = getClass().getResourceAsStream("/testNominations.csv");
+    UpsertResult upsertResult = producer.upsertStream("testupdate", HttpLowLevel.CSV_TYPE, inputStream);
---
+++ src/test/java/com/socrata/ExamplesTest.java
@@ -0,0 +1,103 @@
+package com.socrata;
+
+import com.socrata.api.HttpLowLevel;
+import com.socrata.api.Soda2Consumer;
+import com.socrata.api.Soda2Producer;
+import com.socrata.builders.SoqlQueryBuilder;
+import com.socrata.exceptions.SodaError;
+import com.socrata.model.Meta;
+import com.socrata.model.UpsertResult;
+import com.socrata.model.soql.OrderByClause;
+import com.socrata.model.soql.SoqlQuery;
+import com.socrata.model.soql.SortOrder;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+import junit.framework.TestCase;
+import org.junit.Test;
+import test.model.Nomination;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+/**
+ */
+public class ExamplesTest
+{
+
+    @Test
+    public void readmeConsumerExamples() throws Exception {
+
+        Soda2Consumer consumer = Soda2Consumer.newConsumer("https://sandbox.demo.socrata.com", "testuser@gmail.com", "OpenData", "D8Atrg62F2j017ZTdkMpuZ9vY");
+
+        //To get a raw String of the results
+        ClientResponse response = consumer.getHttpLowLevel().query("nominationsCopy", HttpLowLevel.JSON_TYPE, SoqlQuery.SELECT_ALL);
+        String payload = response.getEntity(String.class);
+        System.out.println(payload);
+
+        //Get get this automatcally serialized into a set of Java Beans annotated with Jackson JOSN annotations
+        List<Nomination> nominations = consumer.query("nominationsCopy", SoqlQuery.SELECT_ALL, Nomination.LIST_TYPE);
+        TestCase.assertTrue(nominations.size() > 0);
+        System.out.println(nominations.size());
+
+
+        //Create a SoQL query to find the nominations for the Department of State
+        SoqlQuery   departmentOfStateQuery = new SoqlQueryBuilder()
+                .addSelectPhrase("name")
+                .addSelectPhrase("position")
+                .addSelectPhrase("nomination_date")
+                .setWhereClause("agency_name='Department of State'")
+                .addOrderByPhrase(new OrderByClause(SortOrder.Descending, "position"))
+                .build();
+        nominations = consumer.query("nominationsCopy", departmentOfStateQuery, Nomination.LIST_TYPE);
+        TestCase.assertTrue(nominations.size() > 0);
+        System.out.println(nominations.size());
+    }
+
+    public static final String NOMINATION_STREAM_TO_ADD = "{\n" +
+            "  \"agency_name\" : \"Department of Imagination\",\n" +
+            "  \"nomination_date\" : \"2009-07-07T07:00:00.000\",\n" +
+            "  \"confirmed\" : true,\n" +
+            "  \"position\" : \"Imaginary Friend\",\n" +
+            "  \"confirmation_vote\" : \"2009-10-05T14:00:00.000Z\",\n" +
+            "  \"name\" : \"Bro, Ney\",\n" +
+            "}";
+
+    public static final Nomination NOMINATION_TO_ADD = new Nomination(
+            "New, User", "Imaginary Friend", "Department of Imagination", null, new Date(), null, null, null
+    );
+
+    public static final Nomination NOMINATION_TO_UPDATE = new Nomination(
+            "New, User", "Imaginary Friend", "Department of Imagination", null, new Date(), new Date(), true, null
+    );
+
+    @Test
+    public void readmeProducerExamples() throws Exception {
+
+        Soda2Producer producer = Soda2Producer.newProducer("https://sandbox.demo.socrata.com", "testuser@gmail.com", "OpenData", "D8Atrg62F2j017ZTdkMpuZ9vY");
+
+        //Get get this automatically serialized into a set of Java Beans annotated with Jackson JOSN annotations
+        Meta nominationAddedMeta = producer.addObject("testupdate", NOMINATION_TO_ADD);
+
+        //Update the nomination
+        Meta nominationUpdatedMeta = producer.update("testupdate", nominationAddedMeta.getId(), NOMINATION_TO_UPDATE);
+
+        //Delete the nomination
+        producer.delete("testupdate", nominationUpdatedMeta.getId());
+    }
+
+    @Test
+    public void upsertExamples() throws Exception {
+
+        Soda2Producer producer = Soda2Producer.newProducer("https://sandbox.demo.socrata.com", "testuser@gmail.com", "OpenData", "D8Atrg62F2j017ZTdkMpuZ9vY");
+
+        InputStream inputStream = getClass().getResourceAsStream("/testNominations.csv");
+        UpsertResult upsertResult = producer.upsertStream("testupdate", HttpLowLevel.CSV_TYPE, inputStream);
+        TestCase.assertEquals(2, upsertResult.getRowsCreated());
+
+    }
+
+}
